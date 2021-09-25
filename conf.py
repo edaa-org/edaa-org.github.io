19f14102835c2fc1224363ee46296058c06a1753
umarcor <unai.martinezcorral@ehu.eus>
2021-09-25 21:50:48 +0200

    add intersphinx target pySystemVerilogModel

--- conf.py
+++ conf.py
@@ -179,6 +179,7 @@ intersphinx_mapping = {
 	'python':               ('https://docs.python.org/3', None),
 	'osvb':                 ('https://umarcor.github.io/osvb', None),
 	'pyVHDLModel':          ('https://vhdl.github.io/pyVHDLModel', None),
+	'pySystemVerilogModel': ('https://edaa-org.github.io/pySystemVerilogModel', None),
 }
 
 
