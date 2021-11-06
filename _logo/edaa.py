c1b250cf4b3233b319195b783baeb8811d2bb2bf
umarcor <unai.martinezcorral@ehu.eus>
2021-11-06 19:55:30 +0100

    logo: ensure that boxes in the center are drawn last, and add strokes to filled boxes

--- _logo/edaa.py
+++ _logo/edaa.py
@@ -86,27 +86,29 @@ def _draw_highlighted(dwg, row, color, shades, offset=(0, 0), unit=50):
     """
     Draw the per-project one-highlighted-layer EDAA logo, with strokes matching the dark colour of the tuple.
     """
-    for x in range(6):
-        for params in [
-            [
-                2 * unit,
-                shades[0][color] if x == row else None
-            ],
-            [
-                4 * unit * (x % 2),
-                shades[1][color] if x == row else None
-            ]
-        ]:
-            dwg.add(
-                dwg.rect(
-                    insert=(offset[0] + params[0], offset[1] + x * unit),
-                    size=(2 * unit, unit),
-                    fill="#ffffff" if params[1] is None else params[1],
-                    fill_opacity=0.0 if params[1] is None else 1.0,
-                    stroke=shades[0][color] if x != row else "",
-                    stroke_width="4px"
-                )
+    def _draw_box(xoffset, fill, stroke):
+        dwg.add(
+            dwg.rect(
+                insert=(offset[0] + xoffset, offset[1] + x * unit),
+                size=(2 * unit, unit),
+                fill="#ffffff" if x != row else fill,
+                fill_opacity=0.0 if x != row else 1.0,
+                stroke=stroke,
+                stroke_width="4px"
             )
+        )
+    for x in range(6):
+        _draw_box(
+            4 * unit * (x % 2),
+            shades[1][color],
+            shades[1][color] if x == row else shades[0][color]
+        )
+    for x in range(6):
+        _draw_box(
+            2 * unit,
+            shades[0][color],
+            shades[0][color]
+        )
 
 def _draw_project_banner(dwg, project, colors, shades, offset=(0, 0), unit=50):
     """
