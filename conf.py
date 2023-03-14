9606a50f80ec7f7387868a087827148f90f3e35a
Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
2023-03-14 22:31:25 +0000

    conf: update theme_overrides.css

--- conf.py
+++ conf.py
@@ -90,6 +90,7 @@ html_css_files = [
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+    "theme_overrides.css",
 ]
 
 html_theme_options = {
